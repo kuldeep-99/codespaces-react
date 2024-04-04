5526295b15dea40e95a23eaf3b7366a58997b7e3
kuldeep-99 <164146307+kuldeep-99@users.noreply.github.com>
2024-04-04 08:19:41 +0000

    new learn

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          Kuldeep Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            I make a new game now.
-          </a>
-        </p>
+        <p>Hi I am kuldeep , learning new thing here</p>
       </header>
     </div>
   );
